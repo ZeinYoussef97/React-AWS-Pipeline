09fd416f4dcdd9a5f79427dd3dc13d7023e3fdeb
zeinyoussef <zein_youssef@hotmail.com>
2024-01-03 13:46:25 -0500

    Specified nodejs version 18 for download on before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,1 @@
 #!/bin/bash
-
-curl -sL https://deb.nodesource.com/setup_18.x -o /tmp/nodesource_setup.sh
-
-# navigate to app folder
-cd /app
-
-# install node and npm
-sudo apt update -y
-sudo apt install nodejs -y
-sudo apt install npm -y
