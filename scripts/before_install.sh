586d63520e9b92327a4cac2b0a4acee1a7dc7f51
zeinyoussef <zein_youssef@hotmail.com>
2024-01-03 12:05:44 -0500

    Updated before install script to use apt

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,6 @@
 cd /app
 
 # install node and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
