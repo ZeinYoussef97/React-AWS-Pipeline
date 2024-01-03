5ed30c77e492d5fa3c1c7a308e3d87c3afbc92e3
zeinyoussef <zein_youssef@hotmail.com>
2024-01-03 11:43:38 -0500

    Added scripts folder

---
+++ scripts/after_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# install dependencies
+npm install
+
+# install create-react-app and react-scripts
+# without react-scripts application cannot be started
+npm install --save create-react-app react-scripts
---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# navigate to app folder
+cd app
+
+# initial startup by running react-script "start", name process "marketing"
+npm run start
